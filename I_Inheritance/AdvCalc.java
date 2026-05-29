c290f26e54ec442576784508248ac99943f3a2f6
proindra <prajwalindrakh@gmail.com>
2026-05-29 18:53:52 +0530

    Fourth commit

---
+++ I_Inheritance/AdvCalc.java
@@ -0,0 +1,9 @@
+public class AdvCalc extends Calc { //even if u dont have Calc.java, u can still use it as long as it only need Clac.class file
+    public int multi(int n1, int n2){
+        return n1 * n2;
+    }
+    public int div(int n1, int n2){
+        return n1 / n2;
+    }
+
+}
---
+++ I_Inheritance/Demo.java
@@ -0,0 +1,13 @@
+public class Demo {
+    public static void main(String a[]){
+        VeryAdvCalc obj = new VeryAdvCalc();
+        
+        int r1 = obj.add(4,5);
+        int r2 = obj.sub(7,3);
+        int r3 = obj.multi(4,5);
+        int r4 = obj.div(7,3);
+        double r5 = obj.power(4,5);
+
+        System.out.println(r1 + " " + r2 + " " + r3 + " " + r4 + " " + r5);
+    }
+}
